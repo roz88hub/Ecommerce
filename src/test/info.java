4cea94665e09f296f6116e96f9919d3063c0db72
roz88hub <41391440+roz88hub@users.noreply.github.com>
2018-09-14 11:10:46 -0400

    Added test.java

---
+++ src/test/info.java
@@ -0,0 +1,15 @@
+package test;
+
+public class info {
+	
+	public int age = 27;
+	
+	String name = "Rojan";
+	
+	protected double price = 1.1;
+	
+	private String university = "UNH";
+	
+	
+
+}
---
+++ src/test/test.java
@@ -0,0 +1,9 @@
+package test;
+import test.Info;
+
+public class test {
+   
+	public static void main(String[] args) {
+		
+	}
+}
